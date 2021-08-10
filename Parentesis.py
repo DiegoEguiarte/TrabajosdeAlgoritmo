c150fd56b08febb0c5127722c0acd7cd38fe8707
DiegoEguiarte <49079314+DiegoEguiarte@users.noreply.github.com>
2021-08-10 10:41:10 -0600

    Add files via upload

---
+++ Parentesis.py
@@ -0,0 +1,16 @@
+def cadena_parentesis_valida(cadena):
+    pila = []
+    parentesis = { '{': '}', '(': ')', '[': ']'}
+
+    for c in cadena:
+        if c in parentesis:
+            pila.append(c)
+        elif len(pila) == 0 or c != parentesis[pila.pop()]:
+            return False
+
+    return len(pila) == 0
+
+print(cadena_parentesis_valida('()()()()()()()()()'))
+print(cadena_parentesis_valida('(((((((((((((((((((((((((((((((((('))
+print(cadena_parentesis_valida('([])[]()'))
+print(cadena_parentesis_valida('(([))]'))
